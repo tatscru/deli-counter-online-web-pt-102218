24c147ac54f18eafe2835c257cfc267392228b9e
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-29 23:10:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def line(katz_deli)
+ katz_deli = []  
+    "The line is currently empty."
+end 
+
+# def take_a_number(katz_deli, name)
+#   name.each {|name, katz_deli| "Welcome, #{name}. You are number #{katz_deli} in line"
+# end 
+
+def now_serving(katz_deli)
+  puts 
+end 
+
