91f0539c1aa6de35ceeb74e27cccd9952024f532
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-29 23:23:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,11 +10,11 @@ def line(array)
 end 
 end 
 
-def take_a_number(array)
-  name.each_with_index {|name, index| "Welcome, #{name}. You are number #{index + 1} in line."
-end 
+# def take_a_number(array)
+#   name.each_with_index {|name, index| "Welcome, #{name}. You are number #{index + 1} in line."
+# end 
 
-def now_serving(katz_deli)
-  puts 
-end 
+# def now_serving(katz_deli)
+#   puts 
+# end 
 
