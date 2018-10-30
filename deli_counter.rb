bea10be4b1cdfc762c89221a7ddebae3b968fc52
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 00:01:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,12 @@
 # Write your code here.
 katz_deli = []
 
-
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
 
 
 # def line(array)
