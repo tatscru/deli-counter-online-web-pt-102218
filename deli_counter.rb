d95097fab1796d73360a89f165f8407fa94168d0
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 00:00:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,8 +11,20 @@ def line(array)
   end 
   puts "#{message}"
 end 
+end 
 
-
+# def line(array) 
+#   if array.length == 0 
+#     puts "The line is currently empty."
+#   else 
+#     message = "The line is currently:"
+    
+#   array.each_with_index do |value, index| 
+#     message += " #{index.to_i+1}. #{value}"
+#   end 
+#   puts "#{message}"
+#   end 
+# end 
 
 
 
