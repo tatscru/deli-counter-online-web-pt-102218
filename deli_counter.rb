92b21b1cc8a35c2206954c4366767e8dea8fe3f4
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 00:26:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
   if array.length = 0
     puts "The line is currently empty."
   else
-    array.each.with_index(1) do |name, index|
+    array.each_with_index(1) do |name, index|
       line_array.push("#{index}. #{name}")
     end
     puts "The line is currently: #{line_array.join(" ")}"
