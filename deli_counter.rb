69e1aa27a539652ed7943816b67b53cf2a44dcc7
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 00:36:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,31 @@
 # Write your code here.
 katz_deli = []
 
-def line(array)
-  array = []
-  if array.length == 0
+# def line(array)
+#   array = []
+#   if array.length == 0
+#     puts "The line is currently empty."
+#   else
+#     message = "The line is currently: array.index(name)+1"
+#     array.each_with_index  do |name, index|
+#     message += " #{index.to_i+1}. #{name}"
+#     end
+#     puts "#{message}"
+#   end
+# end
+
+def line(array) 
+  if array.length == 0 
     puts "The line is currently empty."
-  else
-    message = "The line is currently: array.index(name)+1"
-    array.each_with_index  do |name, index|
-    message += " #{index.to_i+1}. #{name}"
-    end
-    puts "#{message}"
-  end
-end
+  else 
+    message = "The line is currently:"
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end 
 
 def take_a_number(array, name)
   array.push(name)
