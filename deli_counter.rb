e6469c3fe30f8568de53ee059913dd48d3cc34f1
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 00:26:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 
 def line(array)
   array = []
-  if array.length = 0
+  if array.length == 0
     puts "The line is currently empty."
   else
     array.each_with_index(1) do |name, index|
