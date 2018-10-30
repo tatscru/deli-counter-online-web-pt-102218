7a2a43667d414676d46d827e2292aa2ca6cab8a6
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 00:13:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ katz_deli = []
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.index +1} in line."
 end
 
 
